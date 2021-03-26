99758ccd7199aa5b866acd38fd5cfd32dd979eba
Ying Liu <yingl1984@gmail.com>
2021-03-25 18:36:18 -0700

    deploy

--- src/server.js
+++ src/server.js
@@ -9,6 +9,6 @@ const db = knex({
 
 app.set('db', db);
 
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening at http://localhost:${process.env.PORT}`);
 });
