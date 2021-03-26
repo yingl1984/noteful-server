0ad81a95e4f0dd1145f057808a7a8e0a8de39759
Ying Liu <yingl1984@gmail.com>
2021-03-25 19:10:55 -0700

    deploy

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const knex = require('knex');
 const app = require('./app.js');
 const {PORT,DATABASE_URL} = require('./config.js');
-
+// 325
 const db = knex({
   client: 'pg',
   connection: "postgres://hjmrhidldqjlac:735e546cb2ecd38acd81afc1d21caeb85b50d563b6e5ccee45fe740b85ae1ed9@ec2-23-21-229-200.compute-1.amazonaws.com:5432/d9ruaatoggduqh?ssl=true"
