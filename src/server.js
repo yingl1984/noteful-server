a4f343109b52ff714841c4c6adce299203f8a1b2
Ying Liu <yingl1984@gmail.com>
2021-03-25 18:34:02 -0700

    deploy

--- src/server.js
+++ src/server.js
@@ -1,15 +1,13 @@
 const knex = require('knex');
 const app = require('./app.js');
-const config = require('./config.js');
-const PORT = config.PORT;
-//, DATABASE_URL 
+const {PORT,DATABASE_URL} = require('./config.js');
 
-// const db = knex({
-//   client: 'pg',
-//   connection: DATABASE_URL
-// });
+const db = knex({
+  client: 'pg',
+  connection: DATABASE_URL
+});
 
-// app.set('db', db);
+app.set('db', db);
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
